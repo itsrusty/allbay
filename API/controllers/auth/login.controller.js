f7147194aa2aa2b95771c2302d7cddcbf1d24cff
itsrusty <rusty3031@gmail.com>
2023-05-15 03:28:07 -0700

    add controllers

--- API/controllers/auth/login.controller.js
+++ API/controllers/auth/login.controller.js
@@ -1,3 +1,8 @@
-export const login = async (req, res)=>{
-    res.send("/login")
-}
+import bcrypt from "bcrypt";
+import { Pool } from "../../config/conex.js";
+
+export const login = async (req, res) => {
+  res.json({
+    "message": "testing"
+  })
+};
--- API/controllers/file/uploadFile.controller.js
+++ API/controllers/file/uploadFile.controller.js
@@ -1,3 +1,12 @@
-export const upload = (req, res)=>{
-    res.send("hello world!");
-}
+// import multerConfig from "../../middleware/multer.config.js"
+export const upload = async (req, res) => {
+  try {
+    console.log(req.files)
+    res.json({
+      "message": "Upload!"
+    })
+    
+  } catch (error) {
+    res.status(500).json({ "error": error });
+  }
+};
